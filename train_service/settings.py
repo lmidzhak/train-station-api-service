35f4d31af6c286af4ab90b8cd9d8a71b812fcfa6
Liubomyr Midzhak <lmidzhak@gmail.com>
2024-08-28 23:09:23 +0300

    Add Pillow to requirements.txt; MEDIA_ROOT, MEDIA_URL to settings.py

--- train_service/settings.py
+++ train_service/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = "/files/media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
